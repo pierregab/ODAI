af6a12ef277bfc63377d8790d845a9ab914ab97e
Pierregab <pg.bibal@gmail.com>
2024-02-12 12:38:55 +0100

    Update lens material and make thicknesses and radii variable

--- main_classes/triplet_study2.py
+++ main_classes/triplet_study2.py
@@ -23,12 +23,16 @@ for seq_file_path in seq_file_paths:
     # Change the material of the three lenses
     for lens_number, material in material_dict.items():
         surface_number = 2 * lens_number  # Assuming lenses are at even-numbered surfaces
-        optical_system.get_surface(surface_number).set_material(material)
+        optical_system.cv.Command(f"GL1 S{surface_number} {material}")
     
     # Make all thicknesses and radii variable
-    optical_system.make_all_thicknesses_variable()
-    optical_system.make_all_radii_variable()
+    for i in range(1, len(material_dict)):
+        optical_system.cv.Command(f"CCY S{i} 0")    
+        i+=1
 
+    for i in range(1, len(material_dict)):
+        optical_system.cv.Command(f"THC S{i} 0")
+        
     # Perform optimization
     optical_system.optimize_system(efl=1)  # Replace 'efl=1' with the desired effective focal length
     
